35c66c122749606959a817d423add4579872c830
Piers <solar_1992@hotmail.co.uk>
2013-01-23 08:31:30 +0000

    Starting AI

---
+++ 301/src/Learning/Towers/AI/Commander.java
@@ -0,0 +1,68 @@
+package Learning.Towers.AI;
+
+import Learning.Towers.Faction;
+import Learning.Towers.Influence.InfluenceMap;
+
+/**
+ * Primary AI class
+ * <p/>
+ * Gives the actual orders
+ */
+public class Commander {
+
+    private Faction faction;
+    private InfluenceMap influence;
+
+    private AttackFinder attackFinder;
+    private DefenseFinder defenseFinder;
+
+    private void update() {
+        attackFinder.update();
+        defenseFinder.update();
+    }
+}
+
+abstract class TacticalAnalysis {
+    private int tickFrequency, currentTick;
+
+    private Commander commander;
+
+    public TacticalAnalysis(int tickFrequency) {
+        this.tickFrequency = tickFrequency;
+    }
+
+    public final void update() {
+        currentTick = (currentTick == tickFrequency) ? 0 : currentTick + 1;
+        updateSpecialisation();
+    }
+
+    public abstract void updateSpecialisation();
+}
+
+/**
+ * This will churn out attack orders
+ */
+class AttackFinder extends TacticalAnalysis {
+
+    AttackFinder(int tickFrequency) {
+        super(tickFrequency);
+    }
+
+    public void updateSpecialisation() {
+
+    }
+}
+
+/**
+ * This class will locate the next location to build a tower
+ */
+class DefenseFinder extends TacticalAnalysis {
+
+    DefenseFinder(int tickFrequency) {
+        super(tickFrequency);
+    }
+
+    public void updateSpecialisation() {
+
+    }
+}
