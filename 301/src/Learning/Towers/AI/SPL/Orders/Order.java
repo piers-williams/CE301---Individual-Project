93af6c330e25ae476776c36234d0fc98c7867c8b
Piers <solar_1992@hotmail.co.uk>
2013-01-24 10:04:18 +0000

    Added Arguments to SPL Order

--- 301/src/Learning/Towers/AI/SPL/Orders/Order.java
+++ 301/src/Learning/Towers/AI/SPL/Orders/Order.java
@@ -7,5 +7,10 @@ public interface Order extends Comparable<Order>{
 
     public String getType();
 
+    // Completely un type safe but small enough cases I can probably handle it
+    // Switching on strings is rather useful here
+    public void setArguments(Object... args);
+    public Object[] getArguments();
+
     public double getPriority();
 }
