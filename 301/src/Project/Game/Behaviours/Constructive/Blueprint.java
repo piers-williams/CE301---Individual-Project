173b216770e937e757b9c261c90c1f005bff4993
Piers <solar_1992@hotmail.co.uk>
2013-02-08 10:44:40 +0000

    More work done on XML Blueprints

--- 301/src/Project/Game/Behaviours/Constructive/Blueprint.java
+++ 301/src/Project/Game/Behaviours/Constructive/Blueprint.java
@@ -2,21 +2,24 @@ package Project.Game.Behaviours.Constructive;
 
 import Project.Game.Vector2D;
 
-import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import java.util.ArrayList;
 
 /**
- *  Blueprints for Bases
+ * Blueprints for Bases
  */
 @XmlRootElement(name = "Base")
 public class Blueprint {
 
+    @XmlElement(name = "Upgrade")
+    Blueprint upgrade;
+
+    @XmlElement(name = "Buildings")
     ArrayList<Building> buildings;
 }
 
-class Building{
+class Building {
     // Type of the building
     @XmlElement(name = "Type")
     String type;
