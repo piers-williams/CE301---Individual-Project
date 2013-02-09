fa06524bf1d0eb54dabecd2e2d2ccf2e84fa0413
Piers <solar_1992@hotmail.co.uk>
2013-02-09 11:24:26 +0000

    XML parsing is getting much better

--- 301/src/Project/Game/Blueprints/Blueprint.java
+++ 301/src/Project/Game/Blueprints/Blueprint.java
@@ -3,28 +3,32 @@ package Project.Game.Blueprints;
 import Project.Game.Vector2D;
 
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
 import java.util.ArrayList;
 
 /**
  * Blueprints for Bases
  */
-@XmlRootElement(name = "Base")
+@XmlRootElement
 public class Blueprint {
 
-    @XmlElement(name = "Upgrade")
     Blueprint upgrade;
+    @XmlElement(name = "FullName")
+    String name;
 
-    @XmlElement(name = "Buildings")
+    @XmlElementWrapper(name = "Buildings")
+    @XmlElement(name = "Building")
     ArrayList<Building> buildings;
 }
 
+@XmlRootElement
 class Building {
     // Type of the building
     @XmlElement(name = "Type")
     String type;
     // Offset x, y
     @XmlElement(name = "Offset")
-    Vector2D x, y;
+    Vector2D offset;
 
 }
--- 301/src/Project/Game/Blueprints/BlueprintRegistry.java
+++ 301/src/Project/Game/Blueprints/BlueprintRegistry.java
@@ -2,17 +2,28 @@ package Project.Game.Blueprints;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
+import java.io.File;
 import java.util.ArrayList;
 
 /**
  * Stores and loads the Blueprints for the game
  */
-@XmlRootElement(name = "Bases")
+@XmlRootElement(name = "Registry")
 public class BlueprintRegistry {
 
     // Blueprints are the same for every player/faction
-    private ArrayList<Blueprint> blueprints;
+    @XmlElementWrapper(name = "Bases")
+    @XmlElement(name = "Base")
+    public ArrayList<Blueprint> blueprints;
+
+    public BlueprintRegistry() {
+        blueprints = new ArrayList<>();
+    }
 
     public void loadBlueprints(String filename) {
 
@@ -22,5 +33,19 @@ public class BlueprintRegistry {
             e.printStackTrace();
         }
 
+    }
+
+    public static void main(String[] args) {
+        try {
+            JAXBContext context = JAXBContext.newInstance(BlueprintRegistry.class);
+
+            Unmarshaller unmarshaller = context.createUnmarshaller();
+            BlueprintRegistry registry = (BlueprintRegistry) unmarshaller.unmarshal(new File("Content/Bases/Bases.xml"));
+
+            Marshaller marshaller = context.createMarshaller();
+            marshaller.marshal(registry, System.out);
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
     }
 }
