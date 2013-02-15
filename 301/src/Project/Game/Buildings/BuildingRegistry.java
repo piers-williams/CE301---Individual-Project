f6f18882370cab4f8b751686ac3c0a2dc4c6d0a7
Piers <solar_1992@hotmail.co.uk>
2013-02-15 13:57:40 +0000

    Working on more useful XML data for the game

--- 301/src/Project/Game/Buildings/BuildingRegistry.java
+++ 301/src/Project/Game/Buildings/BuildingRegistry.java
@@ -4,18 +4,18 @@ import java.util.HashMap;
 
 /**
  * Stores and loads the building xml
- *
+ * <p/>
  * Gives access to building stats
  */
 public class BuildingRegistry {
 
     HashMap<String, MetaBuilding> buildings;
 
-    public BuildingRegistry(){
+    public BuildingRegistry() {
         buildings = new HashMap<>();
     }
 
-    public MetaBuilding getBuilding(String name){
-
+    public MetaBuilding getBuilding(String name) {
+        return null;
     }
 }
