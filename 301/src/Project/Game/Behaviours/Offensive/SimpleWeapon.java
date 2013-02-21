2b1fb5c2a31f9a32ecb4881bc131107ee5fad1f9
Piers <solar_1992@hotmail.co.uk>
2013-02-21 08:36:30 +0000

    Wrote large part of the weapon class

--- 301/src/Project/Game/Behaviours/Offensive/SimpleWeapon.java
+++ 301/src/Project/Game/Behaviours/Offensive/SimpleWeapon.java
@@ -16,23 +16,41 @@ public class SimpleWeapon implements Offensive {
     // Location extracted from the entity
     private Vector2D currentLocation;
 
-    private int targetRadius, damage, coolDown;
+    private int targetRadius, damage, coolDown, maxCoolDown;
 
     public SimpleWeapon(Entity entity, int targetRadius, int damage, int coolDown) {
         this.entity = entity;
         this.currentLocation = new Vector2D(entity.getMovementBehaviour().getLocation());
         this.targetRadius = targetRadius;
         this.damage = damage;
-        this.coolDown = coolDown;
+        //this.coolDown = coolDown;
+        this.maxCoolDown = coolDown;
     }
 
     @Override
     public void update() {
         currentLocation.clone(entity.getMovementBehaviour().getLocation());
-        if (coolDown != 0) {
-            ArrayList<Entity> entities = Main.GAME_LOOP.getEntities(currentLocation, targetRadius);
-        }
+        if (coolDown == 0) {
+            ArrayList<Entity> entities = Main.GAME_LOOP.getEntities(currentLocation, targetRadius, entity.getFaction());
+            if (entities.size() > 0) {
 
+                Entity closestEntitySoFar = entities.get(0);
+                double lowestDistanceSoFar = entities.get(0).getMovementBehaviour().getLocation().dist(entity.getMovementBehaviour().getLocation());
+                for (int i = 1; i < entities.size(); i++) {
+                    double thisDistance = entities.get(i).getMovementBehaviour().getLocation().dist(entity.getMovementBehaviour().getLocation());
+                    if (thisDistance < lowestDistanceSoFar) {
+                        lowestDistanceSoFar = thisDistance;
+                        closestEntitySoFar = entities.get(i);
+                    }
+                }
+
+                // Take shot
+                closestEntitySoFar.damage(damage);
+                coolDown = maxCoolDown;
+            }
+        } else {
+            coolDown--;
+        }
     }
 
     @Override
