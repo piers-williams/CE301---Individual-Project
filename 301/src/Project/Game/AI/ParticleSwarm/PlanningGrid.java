df5ca555b5b5f941c3bfc915ec27f0dfebd85b96
Piers <solar_1992@hotmail.co.uk>
2013-02-21 12:59:23 +0000

    Conversion to using Hashmap

--- 301/src/Project/Game/AI/ParticleSwarm/PlanningGrid.java
+++ 301/src/Project/Game/AI/ParticleSwarm/PlanningGrid.java
@@ -4,6 +4,7 @@ import Project.Game.Main;
 import Project.Game.Vector2D;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Stores a grid of allowed points and runs swarms to find best
@@ -11,20 +12,21 @@ import java.util.ArrayList;
  * Might need to thread this one out and use callbacks of a kind
  */
 public class PlanningGrid {
-    // [x][y][allowedType]
-    // allowedType:
-    // 0 = p
-    // 1 = t
-    // 2 = c
-    char[][][] grid;
+
+    // TODO Encode information about whether something already exists here
+    HashMap<Vector2D, ArrayList<Character>> grid;
     int cellSize;
     ArrayList<BasicParticle> particles;
 
+    // Map existing buildings onto the grid using basic collision based on the radius.
+    // assign a shadow to each particle to use to check for collisions with existing buildings
+
     public PlanningGrid(int cellSize, int Rp, int Rt, int Rc) {
         this.cellSize = cellSize;
-        grid = new char[Main.MAP_WIDTH / cellSize][Main.MAP_HEIGHT / cellSize][3];
+//        grid = new char[Main.MAP_WIDTH / cellSize][Main.MAP_HEIGHT / cellSize][4];
+        grid = new HashMap<>();
         particles = new ArrayList<>(300);
-
+        // Perform conversion to cells from actual radius
         constructGrid(Rp / cellSize, Rt / cellSize, Rc / cellSize);
     }
 
@@ -36,23 +38,35 @@ public class PlanningGrid {
      * @param Rc Radius in cells of the Construction bases
      */
     private void constructGrid(int Rp, int Rt, int Rc) {
-        for (int x = 0; x < grid.length; x += Rp) {
-            for (int y = 0; y < grid[x].length; y += Rp) {
-                grid[x][y][0] = 'p';
+        for (int x = 0; x < Main.MAP_WIDTH / cellSize; x += Rp) {
+            for (int y = 0; y < Main.MAP_HEIGHT / cellSize; y += Rp) {
+                grid.put(Main.VECTOR2D_SOURCE.getVector(x, y), new ArrayList<Character>(3));
+                grid.get(Main.VECTOR2D_SOURCE.getVector(x, y)).add('p');
             }
         }
-        for (int x = 0; x < grid.length; x += Rt) {
-            for (int y = 0; y < grid[x].length; y += Rt) {
-                grid[x][y][1] = 't';
+        for (int x = 0; x < Main.MAP_WIDTH / cellSize; x += Rc) {
+            for (int y = 0; y < Main.MAP_HEIGHT / cellSize; y += Rc) {
+                if (!grid.containsKey(Main.VECTOR2D_SOURCE.getVector(x, y))) {
+                    grid.put(Main.VECTOR2D_SOURCE.getVector(x, y), new ArrayList<Character>(3));
+                }
+                grid.get(Main.VECTOR2D_SOURCE.getVector(x, y)).add('c');
             }
         }
-        for (int x = 0; x < grid.length; x += Rc) {
-            for (int y = 0; y < grid[x].length; y += Rc) {
-                grid[x][y][2] = 'c';
+        for (int x = 0; x < Main.MAP_WIDTH / cellSize; x += Rt) {
+            for (int y = 0; y < Main.MAP_HEIGHT / cellSize; y += Rt) {
+                if (!grid.containsKey(Main.VECTOR2D_SOURCE.getVector(x, y))) {
+                    grid.put(Main.VECTOR2D_SOURCE.getVector(x, y), new ArrayList<Character>(3));
+                }
+                grid.get(Main.VECTOR2D_SOURCE.getVector(x, y)).add('t');
             }
         }
     }
 
+    /**
+     * Calculates and returns the location of the global best position of all the other particles
+     *
+     * @return Vector2D the best global position
+     */
     private Vector2D calculateGlobalBest() {
         BasicParticle bestSoFar = null;
         double bestFitnessSoFar = Double.MAX_VALUE;
@@ -69,22 +83,43 @@ public class PlanningGrid {
 }
 
 
-abstract class BasicParticle {
+class BasicParticle {
     Vector2D location;
     Vector2D velocity;
     PlanningGrid grid;
+    FitnessFunction fitnessFunction;
 
-    protected BasicParticle(Vector2D location, Vector2D velocity, PlanningGrid grid) {
+    protected BasicParticle(Vector2D location, Vector2D velocity, PlanningGrid grid, FitnessFunction fitnessFunction) {
         this.location = location;
         this.velocity = velocity;
         this.grid = grid;
+        this.fitnessFunction = fitnessFunction;
     }
 
-    protected void update() {
+    protected void update(Vector2D globalBest) {
 
         // Use new velocity to move
         location.add(velocity);
     }
 
+    protected double fitness() {
+        return fitnessFunction.fitness();
+    }
+
+    /**
+     * Resets the particle to a new location, velocity and fitness function
+     *
+     * @param location        new location to start from
+     * @param velocity        new velocity to begin with
+     * @param fitnessFunction new fitness function to calculate with
+     */
+    protected void reset(Vector2D location, Vector2D velocity, FitnessFunction fitnessFunction) {
+        this.location = location;
+        this.velocity = velocity;
+        this.fitnessFunction = fitnessFunction;
+    }
+}
+
+abstract class FitnessFunction {
     protected abstract double fitness();
 }
