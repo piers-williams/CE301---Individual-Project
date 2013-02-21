61cfe10f51aa2608954f1efda84aca0bb3578eeb
Piers <solar_1992@hotmail.co.uk>
2013-02-21 11:30:54 +0000

    adding more logic to particles

--- 301/src/Project/Game/AI/ParticleSwarm/PlanningGrid.java
+++ 301/src/Project/Game/AI/ParticleSwarm/PlanningGrid.java
@@ -1,11 +1,14 @@
 package Project.Game.AI.ParticleSwarm;
 
 import Project.Game.Main;
+import Project.Game.Vector2D;
 
 import java.util.ArrayList;
 
 /**
  * Stores a grid of allowed points and runs swarms to find best
+ * <p/>
+ * Might need to thread this one out and use callbacks of a kind
  */
 public class PlanningGrid {
     // [x][y][allowedType]
@@ -17,19 +20,35 @@ public class PlanningGrid {
     int cellSize;
     ArrayList<BasicParticle> particles;
 
-    public PlanningGrid(int cellSize){
+    public PlanningGrid(int cellSize) {
         this.cellSize = cellSize;
         grid = new char[Main.MAP_WIDTH / cellSize][Main.MAP_HEIGHT / cellSize][3];
         particles = new ArrayList<>(300);
     }
 
-    private void constructGrid(){
+    private void constructGrid() {
         // Need to know Rp, Rt, Rc
     }
 
 }
 
 
-abstract class BasicParticle{
+abstract class BasicParticle {
+    Vector2D location;
+    Vector2D velocity;
+    PlanningGrid grid;
 
+    protected BasicParticle(Vector2D location, Vector2D velocity, PlanningGrid grid) {
+        this.location = location;
+        this.velocity = velocity;
+        this.grid = grid;
+    }
+
+    protected void update() {
+
+        // Use new velocity to move
+        location.add(velocity);
+    }
+
+    protected abstract double fitness();
 }
