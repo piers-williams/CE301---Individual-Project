04d472e0d93e8ce5c5b67c7c4b6a939790ab12f8
Piers <solar_1992@hotmail.co.uk>
2013-02-28 16:28:21 +0000

    Made UI for testing NLP stuff, starting with that work now

---
+++ 301/src/Project/NLP/UI.java
@@ -0,0 +1,41 @@
+package Project.NLP;
+
+import Project.Game.AI.SPL.Orders.SPLObject;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ */
+public class UI {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("NLP UI");
+        final JTextField field = new JTextField();
+        field.setSize(new Dimension(500, 50));
+        JButton button = new JButton("Process");
+
+        frame.setPreferredSize(new Dimension(500, 100));
+
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Pipeline pipeline = new Pipeline();
+
+                SPLObject object = pipeline.convert(field.getText());
+                System.out.println(object);
+            }
+        });
+
+        frame.setLayout(new GridLayout(2, 0));
+        frame.add(field);
+        frame.add(button);
+
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
