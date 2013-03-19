af1fd970e2aea30154d2a27c5546628aea1299c1
Piers <solar_1992@hotmail.co.uk>
2013-03-19 15:45:21 +0000

    More work on the radiusPatrol stuff

--- 301/src/Project/Game/Behaviours/Movement/RadiusPatrol.java
+++ 301/src/Project/Game/Behaviours/Movement/RadiusPatrol.java
@@ -1,7 +1,10 @@
 package Project.Game.Behaviours.Movement;
 
 import Project.Game.Entities.Entity;
+import Project.Game.Main;
 import Project.Game.Vector2D;
+
+import java.util.ArrayList;
 
 /**
  *
@@ -9,13 +12,39 @@ import Project.Game.Vector2D;
 public class RadiusPatrol extends BasicMovement {
     private Vector2D centerLocation;
     private int radius;
+    private int checkPointIndex;
+    private ArrayList<Vector2D> checkPoints;
 
-    public RadiusPatrol(Entity entity, Vector2D location) {
+    private static final int CHECKPOINT_THRESHOLD = 15;
+    private final static int NUMBER_OF_CHECKPOINTS = 16;
+
+    public RadiusPatrol(Entity entity, Vector2D location, Vector2D centerLocation, int radius) {
         super(entity, location);
+        this.centerLocation = centerLocation;
+        this.radius = radius;
+
+        setUpCheckPoints();
+        checkPointIndex = 0;
+    }
+
+    private void setUpCheckPoints() {
+        checkPoints = new ArrayList<>(NUMBER_OF_CHECKPOINTS);
+        Vector2D temp = new Vector2D(centerLocation, true);
+        // Possibly make this a float or just pick numbers that work in ints nicely
+        for (int i = 0; i < NUMBER_OF_CHECKPOINTS; i++) {
+            temp.rotate(Math.toRadians(360 / NUMBER_OF_CHECKPOINTS));
+            checkPoints.add(Main.VECTOR2D_SOURCE.getVector(temp.x, temp.y));
+        }
     }
 
     @Override
     public void updateSpecialisation() {
-
+        if (location.dist(checkPoints.get(checkPointIndex)) < CHECKPOINT_THRESHOLD) {
+            checkPointIndex = (checkPointIndex + 1 >= checkPoints.size()) ? 0 : checkPointIndex++;
+        }
+        // Calculate move to next point
+        direction = location.getNormalDirectionBetween(checkPoints.get(checkPointIndex));
+        // Move to next point
+        location.add(direction);
     }
 }
